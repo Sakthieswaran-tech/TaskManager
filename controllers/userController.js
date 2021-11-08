16883f49af94683eda049f8725316c8d1d023146
imsakthi <sakthi@gmail.com>
2021-11-08 15:21:16 +0530

    Authentication bug cleared

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,7 +16,7 @@ const getAllUsers=(req,res)=>{
     const sql="SELECT * FROM users"
     con.query(sql,(err,result,field)=>{
         if(err) return res.status(500).json({error:err})
-        else return res.status(200).json({users:result})
+        else return res.status(200).json({user:result})
         }
     )
 }
@@ -76,12 +76,13 @@ const fetchToken=(req,res)=>{
     const sql="SELECT * FROM users WHERE employee_id=?"
     con.query(sql,[req.body.employee_id],(err,result,field)=>{
         if(err) return res.status(500).json({error:err})
+        console.log(result[0].password)
         if(result.length<1) return res.status(404).json({message:"User not found"})
         else{
             console.log(result[0].role);
             bcrypt.compare(req.body.password,result[0].password,(error,resp)=>{
-                if(error) return res.status(401).json({message:"Unauthorized"})
-                else{
+                if(error) return res.status(500).json({error:error})
+                else if(resp){
                     const token=jwt.sign(
                         {
                         employee_id:req.body.employee_id,
@@ -89,6 +90,9 @@ const fetchToken=(req,res)=>{
                     },
                         "SECRET KEY")
                     return res.status(200).json({token:token})
+                }
+                else{
+                    return res.status(401).json({message:"Unauthorized"})
                 }
             })
         }
